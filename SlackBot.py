1ef79f627003a2c1561078742e00da67716b058f
Jang <Younghwan.Jang@Cerner.com>
2017-02-03 15:29:55 -0600

    Iteration004 - File Upload feature added

--- SlackBot.py
+++ SlackBot.py
@@ -1,4 +1,10 @@
 from slackclient import SlackClient
+from urllib import urlencode
+import requests
+SLACK_API_URL = "https://slack.com/api/%s?%s"
+
+def make_url(base, **parms):
+    return SLACK_API_URL % (base, urlencode(parms))
 
 class SlackBot:
     def __init__(self, token=None):
@@ -18,10 +24,20 @@ class SlackBot:
         return None
 
     def send_message(self, receipt, text):
-        if self._username is None or self._emiji is None:
+        if self._username is None or self._emoji is None:
             self._client.api_call("chat.postMessage",channel=receipt,text=text,
                                   as_user=True)
         else:
             self._client.api_call("chat.postMessage",channel=receipt,text=text,
                                   username=self._username, icon_emoji=self._emoji)
+
+    def upload_file(self, receipt, filepath):
+        requrl = make_url("files.upload",token=self._token,
+                          filename=filepath,filetype="png")
+        if self._username is None or self._emoji is None:
+            data= {"channels":[receipt], "as_user":True}
+        else:
+            data= {"channels":[receipt], "username":self._username, "icon_emoji":self._emoji, "as_user":False}
+        resp = requests.post(requrl, files={"file":open(filepath,'rb')}, data=data)
+
 
